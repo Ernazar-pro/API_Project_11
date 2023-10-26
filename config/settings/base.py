b7e2f490a056a54d75bd382b94b21340fc816f81
Jumaniyazov Ernazar <jumaniyazovernazar88@gmail.com>
2023-10-26 21:46:37 +0500

    API_Project_11

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -36,6 +37,18 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework_simplejwt',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=14),
+    "REFRESH_TOKEN_LIFETIME" : timedelta(days=14),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
